a5077812ba4dbc6a8a70e020bd77a4ae6fcde34d
Sjoerd Job Postmus <sjoerdjob@sjec.nl>
2024-12-02 07:48:55 +0100

    Day 01, part B

--- 2024/d01.py
+++ 2024/d01.py
@@ -1,13 +1,18 @@
 import pathlib
 
 
-def a():
+def lists():
     with open("d01.in") as fp:
         pairs = [
             tuple(map(int, line.split()))
             for line in fp
         ]
     left, right = map(list, zip(*pairs))
+    return left, right
+
+
+def a():
+    left, right = lists()
     left.sort()
     right.sort()
     return sum(
@@ -16,5 +21,14 @@ def a():
     )
 
 
+def b():
+    left, right = lists()
+    return sum(
+        l * right.count(l)
+        for l in left
+    )
+
+
 if __name__ == "__main__":
     print(a())
+    print(b())
