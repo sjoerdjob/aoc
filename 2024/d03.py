46e822ed210904a6636b8bd1c629fb441f531644
Sjoerd Job Postmus <sjoerdjob@sjec.nl>
2024-12-03 10:08:05 +0100

    Day 03, part B

--- 2024/d03.py
+++ 2024/d03.py
@@ -12,5 +12,26 @@ def a():
     )
 
 
+def b():
+    mul_on = True
+    total = 0
+    for match in re.finditer(
+        "|".join(
+            [
+                r"(mul)\((?P<multiplier>[0-9]+),(?P<multiplicant>[0-9]+)\)",
+                r"(do|don't)\(\)",
+            ]
+        ),
+        pathlib.Path("d03.in").read_text(),
+    ):
+        if match[4] == "don't":
+            mul_on = False
+        if match[4] == "do":
+            mul_on = True
+        if mul_on and match[1] == "mul":
+            total += int(match["multiplier"]) * int(match["multiplicant"])
+    return total
+
 if __name__ == "__main__":
     print(a())
+    print(b())
